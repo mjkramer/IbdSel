60e3bc3b38d656d3df2ba2f9606d62509bd491e8
Matt Kramer <mkramer@lbl.gov>
2019-10-02 16:34:20 -0700

    Add ClusterSaver

---
+++ ClusterSaver.cc
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "SelectorFramework/core/OutTree.cc"
+
+#include "EventReader.cc"
+#include "MuonSaver.cc"
+
+using Status = Algorithm::Status;
+
+class ClusterTree : public OutTree {
+  static constexpr int NMAX = 2048;
+public:
+  using OutTree::OutTree;
+
+  UChar_t size;
+  UInt_t trigSec[NMAX];
+  UInt_t trigNanoSec[NMAX];
+  Float_t energy[NMAX];
+
+private:
+  void initBranches() override;
+};
+
+void ClusterTree::initBranches()
+{
+  OB(size, "b");
+  tree()->Branch("trigSec", trigSec, "trigSec[size]/F");
+  tree()->Branch("trigNanoSec", trigNanoSec, "trigNanoSec[size]/F");
+  tree()->Branch("energy", energy, "energy[size]/F");
+}
+
+class ClusterSaver : public SimpleAlg<EventReader> {
+  static constexpr float GAPSIZE_US = 1000;
+public:
+  ClusterSaver(int detector);
+  void connect(Pipeline& pipeline) override;
+  Status consume(const EventReader::Data& e) override;
+
+  const short detector;
+
+private:
+  void save();
+
+  ClusterTree singlesTree, clustersTree;
+  std::vector<EventReader::Data> events;
+
+  const MuonSaver* muonSaver;
+};
+
+ClusterSaver::ClusterSaver(int detector) :
+  detector(detector),
+  singlesTree(Form("singles_AD%d", detector)),
+  clustersTree(Form("clusters_AD%d", detector))
+{
+}
+
+void ClusterSaver::connect(Pipeline& pipeline)
+{
+  singlesTree.connect(pipeline);
+  clustersTree.connect(pipeline);
+
+  muonSaver = pipeline.getAlg<MuonSaver>();
+
+  SimpleAlg::connect(pipeline);
+}
+
+Status ClusterSaver::consume(const EventReader::Data& e)
+{
+  if (e.detector != detector ||
+      e.energy < 0.7 ||
+      muonSaver->isMuon())
+    return Status::Continue;
+
+  if (events.size() == 0) {     // Very first event
+    events.push_back(e);
+    return Status::Continue;
+  }
+
+  const Time t_this = e.time();
+  const Time t_last = events.back().time();
+
+  if (t_this.diff_us(t_last) > GAPSIZE_US) {
+    save();
+    events.clear();
+  }
+
+  events.push_back(e);
+
+  return Status::Continue;
+}
+
+void ClusterSaver::save()
+{
+  const int N = events.size();
+  ClusterTree& outTree = N > 1 ? clustersTree : singlesTree;
+  outTree.size = N;
+
+  for (int i = 0; i < N; ++i) {
+    const auto& e = events[i];
+    outTree.trigSec[i] = e.triggerTimeSec;
+    outTree.trigNanoSec[i] = e.triggerTimeNanoSec;
+    outTree.energy[i] = e.energy;
+  }
+
+  outTree.fill();
+}
--- stage1_main.cc
+++ stage1_main.cc
@@ -4,6 +4,8 @@
 #include "EventReader.cc"
 #include "TrigTypeCut.cc"
 #include "MuonSaver.cc"
+#include "FlasherCut.cc"
+#include "ClusterSaver.cc"
 
 const std::vector<std::string> deffiles = {
   "/global/projecta/projectdirs/dayabay/data/dropbox/p17b/lz3.skim.6/recon.Neutrino.0021221.Physics.EH1-Merged.P17B-P._0001.root"
@@ -19,6 +21,11 @@ void stage1_main(int maxEvts=0, const std::vector<std::string>& inFiles = deffil
   p.makeAlg<EventReader>().setMaxEvents(maxEvts);
   p.makeAlg<TrigTypeCut>();
   p.makeAlg<MuonSaver>();
+  p.makeAlg<FlasherCut>();
+
+  for (int detector = 1; detector <= 4; ++detector) {
+    p.makeAlg<ClusterSaver>(detector);
+  }
 
   p.process(inFiles);
 }
