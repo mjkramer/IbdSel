add6efc282d2fc229e1cd822968d21200fd0de56
Matt Kramer <mkramer@lbl.gov>
2019-10-06 19:47:50 -0700

    Add KNL jobs

---
+++ ibd_prod/ibd_job_knl.sl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -A dayabay -C knl -L project,projecta,SCRATCH -q regular -t 6:00:00
+
+# Submit me from my directory! Do a "newenv" first!
+# Make sure ../selector/stage1_main.cc has already been compiled, to avoid race conditions
+
+# submit with submit.sh
+
+module purge
+module load gcc/8.2.0
+module load python/2.7-anaconda-2019.07
+module load gsl/2.5
+source /global/project/projectdirs/dayabay/scratch/mkramer/apps/root-6.18.00-py27/bin/thisroot.sh
+
+echo Running on $(hostname)
+
+sleep $(( RANDOM % 200 ))       # so they don't all mob the lockfile at once on startup (400 jobs, staggered)
+
+export PYTHONPATH=$(pwd)/../common:$PYTHONPATH
+
+srun -n 48 --cpu-bind=cores ./ibd_worker.py -t 5 -c 25 $@
