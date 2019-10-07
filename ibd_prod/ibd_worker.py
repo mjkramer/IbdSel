ecbda4983b8e6262b65530de939b955fb142ead5
Matt Kramer <mkramer@lbl.gov>
2019-10-06 19:47:26 -0700

    Add --chunksize to ibd_worker

--- ibd_prod/ibd_worker.py
+++ ibd_prod/ibd_worker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 
 # Recommend using 50
-CHUNKSIZE = 50              # how many input files to pop off the list at a time
+CHUNKSIZE = 50   # how many input files to pop off the list at a time (default)
 
 import os, time, re, argparse
 from common import ParallelListReader, DoneLogger, parse_path
@@ -11,7 +11,7 @@ def process(path, outdir):
     subdir = runno / 100 * 100
     outpath = os.path.join(outdir, 'EH%d' % site, '%07d' % subdir,
                            'pre_ibd.%07d.%04d.root' % (runno, fileno))
-    os.system('mkdir -p %s' % outpath)
+    os.system('mkdir -p %s' % outpath)  # this actually works because ROOT will delete empty dir when open TFile w/ "RECREATE" lol
     os.system('time root -b -q "../selector/stage1_main.cc+(\\"%s\\", \\"%s\\", %d)"' %
               (path, outpath, site))
 
@@ -20,12 +20,13 @@ def main():
     ap.add_argument('inputlist')
     ap.add_argument('outputdir')
     ap.add_argument('-t', '--timeout', type=float, default=18, help='hours')
+    ap.add_argument('-c', '--chunksize', type=int, default=CHUNKSIZE)
     args = ap.parse_args()
 
     donelist = re.sub(r'\.txt$', '.done.txt', args.inputlist)
 
-    with DoneLogger(donelist, chunksize=CHUNKSIZE) as logger:
-        for path in ParallelListReader(args.inputlist, chunksize=CHUNKSIZE,
+    with DoneLogger(donelist, chunksize=args.chunksize) as logger:
+        for path in ParallelListReader(args.inputlist, chunksize=args.chunksize,
                                        timeout_secs = args.timeout * 3600):
             # sysload()
             process(path, args.outputdir)
