f7d02a52e3b1d6077794e56302eda78fc17147e0
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2019-10-24 16:31:56 +0300

    Finally working version

--- selector/ibd_main.hh
+++ selector/ibd_main.hh
@@ -4,7 +4,8 @@
 #include <string>
 
 static const std::vector<std::string> deffiles = {
- "/global/projecta/projectdirs/dayabay/data/dropbox/p17b/lz3.skim.6/recon.Neutrino.0021221.Physics.EH1-Merged.P17B-P._0001.root"
+ //"/global/projecta/projectdirs/dayabay/data/dropbox/p17b/lz3.skim.6/recon.Neutrino.0021221.Physics.EH1-Merged.P17B-P._0001.root"
+ "inputs/dayabay_p19a_test/recon.Neutrino.0072508.Physics.EH1-Merged.P19A-I._0001.root"
 };
 
 void ibd_main(const std::vector<std::string>& inFiles = deffiles,
