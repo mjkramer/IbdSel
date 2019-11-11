dd25c10f6047f57cacbf9c5027668e0400233ba1
Matt Kramer <mkramer@lbl.gov>
2019-11-10 19:03:14 -0800

    Add actual IBD tree

---
+++ selector/IbdTree.cc
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "SelectorFramework/core/BaseIO.cc"
+
+class IbdTree : public TreeBase {
+public:
+  UInt_t runNo;
+  UInt_t fileNo;
+  UInt_t trigP;
+  UInt_t trigD;
+
+  void initBranches() override;
+};
+
+void IbdTree::initBranches()
+{
+  BR(runNo);
+  BR(fileNo);
+  BR(trigP);
+  BR(trigD);
+}
