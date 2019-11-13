5819d3dd0c2e35721bd09eee81f885efad79685d
Matt Kramer <mkramer@lbl.gov>
2019-11-12 16:01:45 -0800

    Add support for IBDSEL_DEBUG env var

--- selector/tests/test_stage1.C
+++ selector/tests/test_stage1.C
@@ -1,9 +1,10 @@
 void test_stage1(int runno, int fileno, int stage, int site)
 {
+  const char* debug = getenv("IBDSEL_DEBUG") ? "g" : "";
   gROOT->ProcessLine(".x LoadBoost.C");
   gSystem->Load("/usr/common/software/python/2.7-anaconda-2019.07/lib/libsqlite3.so");
   gROOT->ProcessLine(".L tests/FileFinder.cc+");
-  gROOT->ProcessLine(".L stage1_main.cc+g");
+  gROOT->ProcessLine(Form(".L stage1_main.cc+%s", debug));
 
   auto line = Form(".x tests/test_stage1_impl.C(%d, %d, %d, %d)", runno, fileno, stage, site);
 
--- selector/tests/test_stage2.C
+++ selector/tests/test_stage2.C
@@ -1,6 +1,7 @@
 void test_stage2(int stage, int site)
 {
+  const char* debug = getenv("IBDSEL_DEBUG") ? "g" : "";
   gROOT->ProcessLine(".x LoadBoost.C");
-  gROOT->ProcessLine(".L stage2_main.cc+g");
+  gROOT->ProcessLine(Form(".L stage2_main.cc+%s", debug));
   gROOT->ProcessLine(Form(".x tests/test_stage2_impl.C(%d, %d)", stage, site));
 }
