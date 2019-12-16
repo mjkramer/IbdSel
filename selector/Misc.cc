c0f21608d0fd898d92ec93c0fc1daa3be9408752
Matt Kramer <mkramer@lbl.gov>
2019-12-15 16:22:41 -0800

    (trivial) Reformat Misc.cc

--- selector/Misc.cc
+++ selector/Misc.cc
@@ -6,29 +6,31 @@
 #include "Constants.cc"
 
 namespace util {
-  std::vector<Det> ADsFor(Site site, Stage stage)
-  {
-    if (site == Site::EH1) {
-      if (stage == Stage::k7AD)
-        return { Det::AD2 };
-      else
-        return { Det::AD1, Det::AD2 };
-    }
 
-    if (site == Site::EH2) {
-      if (stage == Stage::k6AD)
-        return { Det::AD1 };
-      else
-        return { Det::AD1, Det::AD2 };
-    }
-
-    if (site == Site::EH3) {
-      if (stage == Stage::k6AD)
-        return { Det::AD1, Det::AD2, Det::AD3 };
-      else
-        return { Det::AD1, Det::AD2, Det::AD3, Det::AD4 };
-    }
-
-    throw std::runtime_error("Invalid site/stage in ADsFor");
+std::vector<Det> ADsFor(Site site, Stage stage)
+{
+  if (site == Site::EH1) {
+    if (stage == Stage::k7AD)
+      return { Det::AD2 };
+    else
+      return { Det::AD1, Det::AD2 };
   }
+
+  if (site == Site::EH2) {
+    if (stage == Stage::k6AD)
+      return { Det::AD1 };
+    else
+      return { Det::AD1, Det::AD2 };
+  }
+
+  if (site == Site::EH3) {
+    if (stage == Stage::k6AD)
+      return { Det::AD1, Det::AD2, Det::AD3 };
+    else
+      return { Det::AD1, Det::AD2, Det::AD3, Det::AD4 };
+  }
+
+  throw std::runtime_error("Invalid site/stage in ADsFor");
 }
+
+} // namespace util
