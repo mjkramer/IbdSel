7eb15ca178f12dda0440ffbe2fb6c5e771a515be
Matt Kramer <mkramer@lbl.gov>
2020-01-08 15:05:05 -0800

    Revert "Allow multiple MultCutTools with different muon vetos"
    This reverts commit ecfde3649ad3e3931d014375e5e495129571c523.

--- selector/stage2_main.cc
+++ selector/stage2_main.cc
@@ -44,8 +44,7 @@ void stage2_main(const char* confFile, const char* inFile, const char* outFile,
   // Clock is needed for TimeSyncTool (i.e. all the readers)
   p.makeTool<Clock>();
 
-  p.makeTool<MultCutTool>(MuonAlg::Purpose::ForIBDs);
-  p.makeTool<MultCutTool>(MuonAlg::Purpose::ForSingles);
+  p.makeTool<MultCutTool>();
 
   p.process({inFile});
 
