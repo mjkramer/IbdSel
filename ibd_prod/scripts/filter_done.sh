6fd43a47a96cd260cea4620763d0e5904facb795
Matt Kramer <mkramer@lbl.gov>
2020-01-19 21:49:59 -0800

    Add -r to filter_done.sh (omit in-progress files)

--- ibd_prod/scripts/filter_done.sh
+++ ibd_prod/scripts/filter_done.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+while getopts "p:" opt; do
+    case $opt in
+        p)
+            pending=$OPTARG
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 infile=$1; shift
 
 if [ -z "$infile" ]; then
@@ -7,5 +19,22 @@ if [ -z "$infile" ]; then
     exit 1
 fi
 
-comm -23 <(sort $infile.orig) <(awk '{print $2}' $infile.done | sort) > $infile
+echo "Acquiring lock"
+lockfile -5 $infile.lock
+
+# XXX check this
+if [ -n "$pending" ]; then
+    # the range that we DON'T want because they're being processed
+    end=$(cat $infile.offset)
+    start=$((end - pending))
+
+    head -n $start $infile > $infile.tmp
+    tail -n +$end $infile >> $infile.tmp
+else
+    mv $infile $infile.tmp
+fi
+
+comm -23 <(sort $infile.tmp) <(awk '{print $2}' $infile.done | sort) > $infile
 rm $infile.offset
+
+rm -f $infile.lock
