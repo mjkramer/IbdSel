1149a8dfa22b6fdfd071c13d53395e05c1f76d24
Matt Kramer <mkramer@lbl.gov>
2020-01-19 21:38:38 -0800

    Make LockfileListReader reload the input list when it has changed

--- ibd_prod/python/prod_io.py
+++ ibd_prod/python/prod_io.py
@@ -1,4 +1,6 @@
 import time, os
+from dataclasses import dataclass
+from typing import Optional
 
 class ListReaderBase:
     def __iter__(self):
@@ -35,26 +37,40 @@ class ListWriterBase:
         self.put(f'{tstamp} {line}')
 
 class LockfileListReader(ListReaderBase):
-    def __init__(self, filename, chunksize=1, timeout_secs=None, retry_delay=5):
+    @dataclass
+    class Config:
+        chunksize: int = 1
+        timeout_secs: Optional[int] = None
+        retry_delay: int = 5
+
+    def __init__(self, filename, **cfg_kwargs):
         super().__init__()
 
         self._filename = filename
-        self._chunksize = chunksize
-        self._cache = []
-        self._retry_delay = retry_delay
+        self._cfg = LockfileListReader.Config(**cfg_kwargs)
 
-        self._timeout = timeout_secs
-        if self._timeout:
+        self._all = []
+        self._working = []
+        self._lastmtime = 0
+
+        if self._cfg.timeout_secs:
             self._tstart = time.time()
 
-        self._lines = open(filename).readlines()
+        self._load()
+
+    def _load(self):
+        self._all = open(self._filename).readlines()
+        self._lastmtime = os.path.getmtime(self._filename)
+
+    def _modified(self):
+        return os.path.getmtime(self._filename) > self._lastmtime
 
     def __next__(self):
-        if not self._cache:
-            self._load()
-            if not self._cache:
+        if not self._working:
+            self._pull()
+            if not self._working:
                 raise StopIteration
-        return self._cache.pop(0).strip()
+        return self._working.pop(0).strip()
 
     def _lock_file(self):
         return self._filename + '.lock'
@@ -71,47 +87,55 @@ class LockfileListReader(ListReaderBase):
     def _write_offset(self, offset):
         open(self._offset_file(), 'w').write(str(offset))
 
-    def _load(self):
-        if self._timeout:
+    def _pull(self):
+        if self._cfg.timeout_secs:
             delta = time.time() - self._tstart
-            if delta > self._timeout:
+            if delta > self._cfg.timeout_secs:
                 print('Terminating due to specified timeout')
                 raise StopIteration
 
         print('Grabbing input list lock')
-        os.system('time lockfile -%d %s' % (self._retry_delay, self._lock_file()))
+        os.system('time lockfile -%d %s' % (self._cfg.retry_delay, self._lock_file()))
+
+        if self._modified():
+            self._load()
 
         offset = self._read_offset()
 
-        if offset < len(self._lines):
-            self._write_offset(offset + self._chunksize)
+        if offset < len(self._all):
+            self._write_offset(offset + self._cfg.chunksize)
 
         os.system('rm -f %s' % self._lock_file())
 
-        self._cache = self._lines[offset : offset + self._chunksize]
+        self._working = self._all[offset : offset + self._cfg.chunksize]
 
 class LockfileListWriter(ListWriterBase):
-    def __init__(self, filename, chunksize=1, retry_delay=5):
+    @dataclass
+    class Config:
+        chunksize: int = 1
+        retry_delay: int = 5
+
+    def __init__(self, filename, **cfg_kwargs):
         super().__init__()
 
         self._filename = filename
-        self._chunksize = chunksize
-        self._cache = []
-        self._retry_delay = retry_delay
+        self._cfg = LockfileListWriter.Config(**cfg_kwargs)
+
+        self._buf = []
 
     def _do_put(self, line):
-        self._cache.append(line)
-        if len(self._cache) == self._chunksize:
+        self._buf.append(line)
+        if len(self._buf) == self._cfg.chunksize:
             self._flush()
 
     def close(self):
-        if self._cache:
+        if self._buf:
             self._flush()
 
     def _flush(self):
-        chunk = '\n'.join(self._cache) + '\n'
-        os.system('time lockfile -%d %s.lock' % (self._retry_delay, self._filename))
+        chunk = '\n'.join(self._buf) + '\n'
+        os.system('time lockfile -%d %s.lock' % (self._cfg.retry_delay, self._filename))
         with open(self._filename, 'a') as f:
             f.write(chunk)
         os.system('rm -f %s.lock' % self._filename)
-        self._cache = []
+        self._buf = []
