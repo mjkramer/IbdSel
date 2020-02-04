7473181e83ea44238013f290824bc465481bdf04
Matt Kramer <mkramer@lbl.gov>
2020-02-03 22:27:16 -0800

    Prevent recompilation of stageX_main.cc

--- ibd_prod/cling/run_stage1.C
+++ ibd_prod/cling/run_stage1.C
@@ -2,7 +2,7 @@ void run_stage1(const char* inFile, const char* outFile, int site, int phase)
 {
   gErrorIgnoreLevel = kError;  // suppress warnings of missing NuWa dictionaries
 
-  gROOT->ProcessLine(".L ../selector/stage1_main.cc+");
+  gSystem->Load("../selector/stage1_main_cc.so");
 
   auto form = "stage1_main(\"%s\", \"%s\", Site(%d), Phase(%d))";
   auto line = Form(form, inFile, outFile, site, phase);
