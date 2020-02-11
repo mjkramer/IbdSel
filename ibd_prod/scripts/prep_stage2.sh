5ae2da1f6755c3486a047ae42532d67809f23e3a
Matt Kramer <mkramer@lbl.gov>
2020-02-11 15:51:17 -0800

    Add -c and -b options to prep_stage2.sh

--- ibd_prod/scripts/prep_stage2.sh
+++ ibd_prod/scripts/prep_stage2.sh
@@ -2,10 +2,16 @@
 
 source bash/stage2_vars.inc.sh
 
-while getopts "f:" opt; do
+while getopts "f:cb:" opt; do
     case $opt in
         f)
             filter_cmd=$OPTARG
+            ;;
+        c)
+            create_config=1
+            ;;
+        b)
+            based_on=$OPTARG
             ;;
         *)
             exit 1
@@ -32,8 +38,16 @@ mkdir -p $indir $logdir $trueOutdir
 mkdir -p $(dirname $outdir)
 ln -s $trueOutdir $outdir
 
-cp $IBDSEL_CONFIGDIR/config.$configname.txt $indir
+if [ -n "$create_config" ]; then
+    cp $IBDSEL_CONFIGDIR/config.nominal.txt $indir/config.$configname.txt
+else
+    cp $IBDSEL_CONFIGDIR/config.$configname.txt $indir
+fi
 
-filter_cmd=${filter_cmd:-cat}
-
-python/dump_days.py | $filter_cmd > $infile
+if [ -n "$based_on" ]; then
+    src_infile=$indir/../$tag@$based_on/$(basename $infile)
+    cp $src_infile $infile
+else
+    filter_cmd=${filter_cmd:-cat}
+    python/dump_days.py | $filter_cmd > $infile
+fi
