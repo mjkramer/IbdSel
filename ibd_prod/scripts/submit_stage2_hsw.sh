22d55e23e4e811c144bed99b5df89a3cb2e07f4c
Matt Kramer <mkramer@lbl.gov>
2020-02-27 21:21:49 -0800

    Specify job settings in submit_stage2_foo.sh

--- ibd_prod/scripts/submit_stage2_hsw.sh
+++ ibd_prod/scripts/submit_stage2_hsw.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-export IBDSEL_CHUNKSIZE=YYY
+# Assume best-case 1 node-sec per file and 30 seconds per process.
+# (Reality is 30-60 sec per process, 0.83 node-sec per file)
+# Using a safety factor of 5.
+
+# Min. lockfile period here is 50 * 1sec / njobs
+# So 1Hz for 50 jobs. That's far more jobs than we need. We're good.
+export IBDSEL_CHUNKSIZE=50
 
 export IBDSEL_WALLTIME=02:00:00
-export IBDSEL_CHUNK_MARGIN_SECS=YYY
-export IBDSEL_FILE_MARGIN_SECS=YYY
+export IBDSEL_CHUNK_MARGIN_SECS=250 # 50 * 1sec * 5
+export IBDSEL_FILE_MARGIN_SECS=150  # 30sec * 5
 export IBDSEL_STARTUP_SLEEP_SECS=120
-export IBDSEL_NTASKS=YYY
+export IBDSEL_NTASKS=32
 export IBDSEL_SLURMFILE=slurm/run_hsw.sl.sh
 export IBDSEL_LOGFMT_EXTRA=_hsw
 
--- ibd_prod/scripts/submit_stage2_knl.sh
+++ ibd_prod/scripts/submit_stage2_knl.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-export IBDSEL_CHUNKSIZE=YYY
+# Assume best-case 3 node-sec per file and 90 seconds per process.
+# (Reality is 3-4 minutes per process, 3.6 node-sec per file)
+# Using a safety factor of 5.
+
+# Min. lockfile period here is 50 * 3sec / njobs
+# So 1Hz for 150 jobs. That's far more jobs than we need. We're good.
+export IBDSEL_CHUNKSIZE=50
 
 export IBDSEL_WALLTIME=02:00:00
-export IBDSEL_CHUNK_MARGIN_SECS=YYY
-export IBDSEL_FILE_MARGIN_SECS=YYY
+export IBDSEL_CHUNK_MARGIN_SECS=750 # 50 * 3sec * 5
+export IBDSEL_FILE_MARGIN_SECS=450  # 90sec * 5
 export IBDSEL_STARTUP_SLEEP_SECS=120
-export IBDSEL_NTASKS=YYY
+export IBDSEL_NTASKS=68
 export IBDSEL_SLURMFILE=slurm/run_knl.sl.sh
 export IBDSEL_LOGFMT_EXTRA=_knl
 
