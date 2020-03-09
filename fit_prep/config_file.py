4f7973441a7454f4c7be13874cd9071460157910
Matt Kramer <mkramer@lbl.gov>
2020-03-08 19:57:03 -0700

    Add fit_prep/config_file.py

---
+++ fit_prep/config_file.py
@@ -0,0 +1,27 @@
+"Utilities for handling the SelectorFramework config file format"
+
+class ConfigFile(dict):
+    "Wrapper with a dict interface"
+
+    def __init__(self, path):
+        super().__init__()
+        self._read_from(path)
+
+    def _read_from(self, path):
+        for line in open(path):
+            line = line.strip()
+
+            if line[0] == '#':
+                continue
+
+            key, val = line.split(maxsplit=1)
+
+            try:
+                val = int(val)
+            except ValueError:
+                try:
+                    val = float(val)
+                except ValueError:
+                    pass
+
+            self[key] = val
