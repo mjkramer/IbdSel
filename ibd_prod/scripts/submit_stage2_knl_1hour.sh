a6a517cb397547571b4a08addb2404087091929d
Matt Kramer <mkramer@lbl.gov>
2020-06-16 00:42:17 -0700

    Add submit_stage2_knl_1hour.sh

---
+++ ibd_prod/scripts/submit_stage2_knl_1hour.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Assume best-case 3 node-sec per file and 90 seconds per process.
+# (Reality is 3-4 minutes per process, 3.6 node-sec per file)
+# Using a safety factor of 5.
+
+# Min. lockfile period here is 25 * 3sec / njobs
+# So 1Hz for 75 jobs. That's far more jobs than we need. We're good.
+export IBDSEL_CHUNKSIZE=25
+
+export IBDSEL_WALLTIME=01:00:00
+export IBDSEL_CHUNK_MARGIN_SECS=450 # just use the below
+export IBDSEL_FILE_MARGIN_SECS=450  # 90sec * 5
+export IBDSEL_STARTUP_SLEEP_SECS=60
+export IBDSEL_NTASKS=68
+export IBDSEL_SLURMFILE=slurm/run_knl.sl.sh
+export IBDSEL_LOGFMT_EXTRA=_knl
+export IBDSEL_USE_BURSTBUF=1
+
+source bash/do_submit_stage2.inc.sh
