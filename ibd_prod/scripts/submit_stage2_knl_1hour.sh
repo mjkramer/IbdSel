f17c09521e28cd0cfe989df7791ad8aabde724f1
Matt Kramer <mkramer@lbl.gov>
2020-06-16 13:44:17 -0700

    Increase chunk margin on stage2 knl 1hour job

--- ibd_prod/scripts/submit_stage2_knl_1hour.sh
+++ ibd_prod/scripts/submit_stage2_knl_1hour.sh
@@ -9,7 +9,8 @@
 export IBDSEL_CHUNKSIZE=25
 
 export IBDSEL_WALLTIME=01:00:00
-export IBDSEL_CHUNK_MARGIN_SECS=450 # just use the below
+# previous chunk margin of 450 (== file margin) left us with some stragglers
+export IBDSEL_CHUNK_MARGIN_SECS=600
 export IBDSEL_FILE_MARGIN_SECS=450  # 90sec * 5
 export IBDSEL_STARTUP_SLEEP_SECS=60
 export IBDSEL_NTASKS=68
