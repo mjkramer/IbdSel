9edeaa29c741eec514237ee9340d29724a1bc4c6
Matt Kramer <mkramer@lbl.gov>
2020-12-28 15:12:37 -0800

    Make zmq_fan optional in stage1_worker

--- ibd_prod/python/stage1_worker.py
+++ ibd_prod/python/stage1_worker.py
@@ -8,6 +8,7 @@ import os, argparse, random
 from prod_util import parse_path, sysload, phase_for_run, unbuf_stdout
 from prod_util import stage1_fbf_path, worker_timeout_mins
 from zmq_fan import ZmqListReader, ZmqListWriter
+from prod_io import LockfileListReader, LockfileListWriter
 
 def process(path, tag):
     runno, fileno, site = parse_path(path)
@@ -24,10 +25,20 @@ def process(path, tag):
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('tag')
+    ap.add_argument('-i', '--inputlist',
+                    help="Access the specified input list directly " +
+                    "(via lockfile) instead of using ZMQ fan")
+    ap.add_argument('-c', '--chunksize', type=int, default=1,
+                    help="Only has an effect when used with -i")
     args = ap.parse_args()
 
-    reader = ZmqListReader(timeout_mins=worker_timeout_mins())
-    logger = ZmqListWriter()
+    if args.inputlist:
+        reader = LockfileListReader(args.inputlist, chunksize=args.chunksize)
+        logger = LockfileListWriter(args.inputlist + ".done",
+                                    chunksize=args.chunksize)
+    else:
+        reader = ZmqListReader(timeout_mins=worker_timeout_mins())
+        logger = ZmqListWriter()
 
     with logger:
         for path in reader:
