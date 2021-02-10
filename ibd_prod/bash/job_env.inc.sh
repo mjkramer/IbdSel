7305de31388c7da9c558da16c58bd621adb61a6c
Matt Kramer <mkramer@lbl.gov>
2021-02-09 17:42:38 -0800

    Switch from /global/project to /global/cfs in job_env.inc.sh

--- ibd_prod/bash/job_env.inc.sh
+++ ibd_prod/bash/job_env.inc.sh
@@ -2,7 +2,8 @@ module purge
 module load gcc/8.2.0
 module load python/3.7-anaconda-2019.07
 module load gsl/2.5
-source /global/project/projectdirs/dayabay/scratch/mkramer/apps/root-6.18.00-py37/bin/thisroot.sh
+
+source /global/cfs/projectdirs/dayabay/scratch/mkramer/apps/root-6.18.00-py37/bin/thisroot.sh
 
 eval "$(conda shell.bash hook)"
 conda activate ibdsel1          # $IBDSEL_HOME/doc/conda/ibdsel1.txt
