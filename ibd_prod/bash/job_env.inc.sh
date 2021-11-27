506a9c090d4125db444552c31f185eb9e6fc7716
Matt Kramer <mkramer@lbl.gov>
2021-11-27 11:43:42 -0800

    Switch to ibdsel2

--- ibd_prod/bash/job_env.inc.sh
+++ ibd_prod/bash/job_env.inc.sh
@@ -1,13 +1,22 @@
+# See /global/cfs/cdirs/dayabay/scratch/mkramer/build/NOTES.org for notes on how
+# ROOT and conda were set up on Cori.
+
 module purge
-module load gcc/8.2.0
-module load python/3.7-anaconda-2019.07
-module load gsl/2.5
+module load gcc/10.1.0
+module load cmake/3.21.3
+export CC=gcc CXX=g++ LD=ld
 
-source /global/cfs/projectdirs/dayabay/scratch/mkramer/apps/root-6.18.00-py37/bin/thisroot.sh
+myconda=/global/cfs/projectdirs/dayabay/scratch/mkramer/apps/miniconda3
+export PATH=$myconda/bin:$PATH
+source activate $myconda/envs/ibdsel2 # /doc/conda/ibdsel2.txt
+unset myconda
 
-eval "$(conda shell.bash hook)"
-# /doc/conda/ibdsel1.txt
-conda activate /global/cfs/projectdirs/dayabay/scratch/mkramer/apps/miniconda3/envs/ibdsel1
+# CONDA_PREFIX is $myconda/envs/ibdsel2
+source $CONDA_PREFIX/bin/thisroot.sh
+
+export CFLAGS="-I$CONDA_PREFIX/include $CFLAGS"
+export LIBRARY_PATH=$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
 
 export IBDSEL_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)
 
