5fe88b1505a36c0fe374c227e29a2a8dad25c69a
Matt Kramer <mkramer@lbl.gov>
2021-11-28 10:36:10 -0800

    Switch to ibdsel3 environment

--- ibd_prod/bash/job_env.inc.sh
+++ ibd_prod/bash/job_env.inc.sh
@@ -1,26 +1,28 @@
 # See /global/cfs/cdirs/dayabay/scratch/mkramer/build/NOTES.org for notes on how
-# ROOT and conda were set up on Cori.
+# python, ROOT, and the ibdsel3 virtualenv were set up on Cori.
 
 module purge
 module load gcc/10.1.0
 module load cmake/3.21.3
 export CC=gcc CXX=g++ LD=ld
 
-myconda=/global/cfs/projectdirs/dayabay/scratch/mkramer/apps/miniconda3
-export PATH=$myconda/bin:$PATH
-source activate $myconda/envs/ibdsel2 # /doc/conda/ibdsel2.txt
-unset myconda
+pydir=/global/cfs/cdirs/dune/users/mkramer/apps/mypy310
+rootdir=/global/cfs/cdirs/dayabay/scratch/mkramer/apps/root-6.24.06-mypy310
+venvdir=/global/cfs/cdirs/dayabay/scratch/mkramer/venvs/ibdsel3
 
-# CONDA_PREFIX is $myconda/envs/ibdsel2
-source $CONDA_PREFIX/bin/thisroot.sh
+export PATH=$pydir/bin:$PATH
+export PKG_CONFIG_PATH=$pydir/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$pydir/lib:$LD_LIBRARY_PATH
 
-export CFLAGS="-I$CONDA_PREFIX/include $CFLAGS"
-export LIBRARY_PATH=$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+source $rootdir/bin/thisroot.sh
+source $venvdir/bin/activate
 
 export IBDSEL_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)
 
 export PYTHONPATH=$IBDSEL_HOME/ibd_prod/python:$PYTHONPATH
 export LD_LIBRARY_PATH=$IBDSEL_HOME/selector/_build:$LD_LIBRARY_PATH
 
+export LIBRARY_PATH=$LD_LIBRARY_PATH
 export NO_JUPYROOT=1            # for jupyter console; see NOTES.org
+
+unset pydir rootdir venvdir
